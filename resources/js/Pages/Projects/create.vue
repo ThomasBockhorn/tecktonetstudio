689dcb290abe2b4eb1ad61f123d2ce8d92595c7f
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-11 11:33:34 -0600

    Created a link between create and index

--- resources/js/Pages/Projects/create.vue
+++ resources/js/Pages/Projects/create.vue
@@ -1,12 +1,12 @@
 <template>
-    <Head title="Projects" />
+    <Head title="Create Project" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2
                 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
             >
-                Projects
+                Create Project
             </h2>
         </template>
 
@@ -16,15 +16,15 @@
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        Projects
+                        Create Project
 
                         <div class="mt-4">
                             <Link
-                                :href="route('projects.create')"
+                                :href="route('projects.index')"
                                 active="true"
                                 class="inline-flex items-center px-4 py-2 m-2 bg-gray-800 border border-white rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150"
                             >
-                                Create Project
+                                Back to Projects
                             </Link>
                         </div>
                     </div>
