eef26dd2fee11151130a7274d26756218fd92ede
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-11 11:40:56 -0600

    Updated header in create

--- resources/js/Pages/Project-Images/create.vue
+++ resources/js/Pages/Project-Images/create.vue
@@ -16,7 +16,7 @@
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        Project Images
+                        Create a new Project Image
 
                         <div class="mt-4">
                             <Link
