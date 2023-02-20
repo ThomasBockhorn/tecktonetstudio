63520b28b799cece2e02f5f946e12bb3810cab72
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-19 21:54:59 -0600

    Project form got styled

--- resources/js/Components/BackendForms/BackEndProjectsForm.vue
+++ resources/js/Components/BackendForms/BackEndProjectsForm.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="flex justify-center">
-        <form @submit.prevent="submit">
-            <div class="grid grid-rows-2 mt-5">
-                <label for="project_title">Enter a Project Title:</label>
+    <div class="flex justify-center container">
+        <form
+            @submit.prevent="submit"
+            class="grid grid-rows-3 grid-cols-1 gap-6"
+        >
+            <div class="grid grid-cols-1">
+                <label for="project_title" class=""
+                    >Enter a Project Title:</label
+                >
                 <input
                     class="border border-white rounded-md text-black"
                     type="text"
@@ -12,23 +17,8 @@
                     required
                 />
             </div>
-            <div class="grid grid-rows-2 mt-5">
-                <label class="mt-2" for="project_description"
-                    >Enter a Project Description
-                </label>
-                <input
-                    class="border border-white rounded-md text-black"
-                    type="text"
-                    name="project_description"
-                    id="project_description"
-                    v-model="form.project_description"
-                    required
-                />
-            </div>
-            <div class="grid grid-rows-2 mt-5">
-                <label class="mt-2" for="project_url"
-                    >Enter a Project URL
-                </label>
+            <div class="grid grid-cols-1">
+                <label class="" for="project_url">Enter a Project URL: </label>
                 <input
                     class="border border-white rounded-md text-black"
                     type="text"
@@ -38,7 +28,22 @@
                     required
                 />
             </div>
-            <div class="flex justify-center mt-5">
+            <div class="grid grid-cols-1">
+                <label class="mb-5" for="project_description"
+                    >Enter a Project Description:
+                </label>
+                <textarea
+                    class="border border-white rounded-md text-black"
+                    type="text"
+                    name="project_description"
+                    id="project_description"
+                    v-model="form.project_description"
+                    required
+                    cols="50"
+                ></textarea>
+            </div>
+
+            <div class="flex justify-center">
                 <button
                     class="text-white border px-4 py-2 m-2 bg-gray-800 border border-white rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150"
                     type="submit"
