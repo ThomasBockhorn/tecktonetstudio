7836f87fd5bb9f8b90416de7c2034fa55c614fbb
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-25 08:29:02 -0600

    Created BackendSettingsDropdown and Resposive

---
+++ resources/js/Components/Backend_ui/BackendResponsiveLink.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pt-2 pb-3 space-y-1">
+        <ResponsiveNavLink
+            :href="route('dashboard')"
+            :active="route().current('dashboard')"
+        >
+            Dashboard
+        </ResponsiveNavLink>
+        <ResponsiveNavLink
+            :href="route('skills.index')"
+            :active="route().current('skills.index')"
+        >
+            Skills
+        </ResponsiveNavLink>
+        <ResponsiveNavLink
+            :href="route('projects.index')"
+            :active="route().current('projects.index')"
+        >
+            Projects
+        </ResponsiveNavLink>
+    </div>
+</template>
+
+<script>
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+
+export default {
+    components: {
+        ResponsiveNavLink,
+    },
+};
+</script>
+
+<style></style>
