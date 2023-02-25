8c9b0a1ac7f10e4dcca1880bd1bc87f10337060c
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-25 07:28:33 -0600

    Created a backendlogo component

---
+++ resources/js/Components/Backend_ui/BackendLogo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="shrink-0 flex items-center">
+        <Link :href="route('dashboard')">
+            <ApplicationLogo
+                class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+            />
+        </Link>
+    </div>
+</template>
+
+<script>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        ApplicationLogo,
+        Link,
+    },
+};
+</script>
+
+<style></style>
--- resources/js/Components/Backend_ui/BackendNavigation.vue
+++ resources/js/Components/Backend_ui/BackendNavigation.vue
@@ -7,13 +7,7 @@
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
-                        <Link :href="route('dashboard')">
-                            <ApplicationLogo
-                                class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                            />
-                        </Link>
-                    </div>
+                    <BackendLogo />
 
                     <!-- Navigation Links -->
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -185,22 +179,23 @@
 
 <script>
 import { ref } from "vue";
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
+import BackendLogo from "@/Components/Backend_ui/BackendLogo.vue";
 
 const showingNavigationDropdown = ref(false);
+
 export default {
     components: {
-        ApplicationLogo,
         Dropdown,
         DropdownLink,
         NavLink,
         ResponsiveNavLink,
         Link,
+        BackendLogo,
     },
     setup() {
         return {
