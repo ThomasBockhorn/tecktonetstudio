9804a6c4bf09a168f8bca2b41af9c4b42566dba2
Thomas Bockhorn <historian.thomas@gmail.com>
2023-02-28 04:42:09 -0600

    sticky footer

--- resources/js/Components/Frontend_ui/FrontendFooter.vue
+++ resources/js/Components/Frontend_ui/FrontendFooter.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="absolute bottom-0 w-full">
-        <div class="flex flex-row-reverse m-5">
-            <h1 class="text-gray-900 dark:text-gray-100">
-                &copy; 2023 tecktonet.com
-            </h1>
-        </div>
+    <div class="block fixed bottom-0 z-20 left-0">
+        <h1 class="text-gray-900 dark:text-gray-100">
+            &copy; 2023 tecktonet.com
+        </h1>
     </div>
 </template>
 
