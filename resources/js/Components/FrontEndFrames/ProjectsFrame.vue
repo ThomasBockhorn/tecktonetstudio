3b8217c0f7b185de1fffb4ecd632c4e6cf99f7ab
Thomas Bockhorn <historian.thomas@gmail.com>
2023-03-10 04:55:57 -0600

    changed the overflow property

--- resources/js/Components/FrontEndFrames/ProjectsFrame.vue
+++ resources/js/Components/FrontEndFrames/ProjectsFrame.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="col-span-3 p-7">
+    <div class="col-span-3 p-7 overflow-scroll">
         <h1 class="text-2xl mb-5 text-center uppercase">Projects</h1>
 
-        <ul class="flex flex-col">
-            <li
-                v-for="project in projects"
-                :key="project.id"
-                class="text-center"
-            >
+        <div class="flex flex-col">
+            <div v-for="project in projects" :key="project.id">
+                <h3 class="text-center uppercase mt-5">
+                    {{ project.project_title }}
+                </h3>
+                <div id="neon" class="w-1/4 mx-auto"></div>
+                <p class="mt-5">{{ project.project_description }}</p>
                 <a :href="project.project_url" target="_blank">
                     <span class="hover:underline hover:decoration-2">
-                        {{ project.project_title }}
+                        Visit Website
                     </span>
                 </a>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,6 +28,13 @@ export default {
 </script>
 
 <style scoped>
+#neon {
+    border: 1px solid hsl(186 100% 69%);
+    box-shadow: 0px 0px 0.5em 0px hsl(186 100% 69%),
+        0px 0px 0.5em 0px hsl(186 100% 69%);
+    animation: glow 3s infinate;
+}
+
 span {
     text-decoration-color: hsl(186 100% 69%);
 }
