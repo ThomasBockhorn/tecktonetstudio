d0582eae55e7ae643734b5c77c991ba8d7bf6132
Thomas Bockhorn <historian.thomas@gmail.com>
2023-03-11 09:19:50 -0600

    Added contact info

--- resources/js/Components/FrontEndFrames/ContactFrame.vue
+++ resources/js/Components/FrontEndFrames/ContactFrame.vue
@@ -2,7 +2,15 @@
     <div class="col-span-3 p-7">
         <h1 class="text-2xl mb-5 text-center uppercase">Contact</h1>
         <div class="flex">
-            <div>Contact will be added soon.</div>
+            <div>
+                <p class="text-lg">
+                    If you want to know more about me and how I can help you
+                    build a web solution, you can contact me at:
+                </p>
+                <p class="text-center mt-5 text-lg">
+                    <em>Thomas.Bockhorn@tecktonet.com</em>
+                </p>
+            </div>
         </div>
     </div>
 </template>
