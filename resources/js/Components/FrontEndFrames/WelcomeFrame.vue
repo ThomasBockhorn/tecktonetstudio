72e58e4d0d678f8e35f0731b9fb87c31b030fe35
Thomas Bockhorn <historian.thomas@gmail.com>
2023-03-11 09:46:39 -0600

    Updated paragraph in welcomeframe

--- resources/js/Components/FrontEndFrames/WelcomeFrame.vue
+++ resources/js/Components/FrontEndFrames/WelcomeFrame.vue
@@ -9,15 +9,15 @@
                     class="float-left w-40 h-50 m-5"
                 />
                 <p class="text-lg">
-                    I first got into web development with an invitation by
-                    Udacity including a Google scholarship for their front-end
-                    web development program. After taking a few lessions, I
-                    became hooked on all things web development. To further my
-                    education, I took and continue to take Udemy courses for
-                    both front-end and back-end development. The beauty of both
-                    technical and asthetic spheres and how they interacted
-                    together fascinated me. Just like architecture, good
-                    websites need to consider both form and function.
+                    I first got into web development with an invitation from
+                    Udacity to apply for a Google scholarship for their
+                    front-end web development program. After taking a few
+                    lessons , I became hooked on all things web development. To
+                    further my education, I took and continue to take Udemy
+                    courses for both front-end and back-end development. The
+                    beauty of both technical and asthetic spheres and how they
+                    interact together fascinates me. Just like architecture,
+                    good websites need to consider both form and function.
                 </p>
                 <p class="mt-5">- Thomas Bockhorn</p>
             </div>
