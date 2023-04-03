386d61e67d10f36b62a1333958be287217d7e694
Thomas Bockhorn <historian.thomas@gmail.com>
2023-04-03 16:38:59 -0500

    Updated footer

--- resources/js/Components/Frontend_ui/FrontendFooter.vue
+++ resources/js/Components/Frontend_ui/FrontendFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="block fixed bottom-1 z-20 left-0">
         <h1 class="text-gray-900 dark:text-gray-100">
-            &copy; 2023 tecktonet.com
+            &copy; 2023 ThomasBockhorn.dev
         </h1>
     </div>
 </template>
