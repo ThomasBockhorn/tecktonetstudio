52ad50c84c20b8bdd3a4dab8c8f69e97eb6e3996
Thomas Bockhorn <historian.thomas@gmail.com>
2023-04-03 20:40:13 -0500

    Fixed some css issues

--- resources/js/Components/Frontend_ui/FrontendFooter.vue
+++ resources/js/Components/Frontend_ui/FrontendFooter.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="block fixed bottom-0 z-20 left-0">
-        <h1 class="text-gray-900 dark:text-gray-100">
-            &copy; 2023 ThomasBockhorn.dev
-        </h1>
+    <div class="fixed bottom-0 z-20 left-0">
+        <h1 class="dark:text-gray-100">&copy; 2023 ThomasBockhorn.dev</h1>
     </div>
 </template>
