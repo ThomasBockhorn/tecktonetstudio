1ad00dc76065686cd4f59bae91f99b6f90365092
Thomas Bockhorn <historian.thomas@gmail.com>
2023-04-04 18:43:29 -0500

    updated layout css

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg-gray-900"
+        class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-900"
     >
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
