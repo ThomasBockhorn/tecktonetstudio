a9825bf79d16e3a7d68daab029b4f76a208c120e
Thomas Bockhorn <historian.thomas@gmail.com>
2023-04-09 13:23:23 -0500

    Fixed some backend styling

--- resources/js/Components/Backend_ui/BackendResponsiveSettings.vue
+++ resources/js/Components/Backend_ui/BackendResponsiveSettings.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+    <div class="pt-4 pb-1 border-t border-gray-600">
         <div class="px-4">
-            <div class="font-medium text-base text-gray-800 dark:text-gray-200">
+            <div class="font-medium text-base text-gray-200">
                 {{ $page.props.auth.user.name }}
             </div>
             <div class="font-medium text-sm text-gray-500">
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100 bg-gray-900">
+        <div class="min-h-screen bg-gray-900">
             <BackendNavigation />
             <!-- Page Heading -->
             <header class="bg-gray-800 shadow" v-if="$slots.header">
