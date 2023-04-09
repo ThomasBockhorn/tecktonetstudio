ca4039ba261c09bc8bd24018792ef6b0accafca7
Thomas Bockhorn <historian.thomas@gmail.com>
2023-04-09 11:55:54 -0500

    Updated footer

--- resources/js/Components/Frontend_ui/FrontendFooter.vue
+++ resources/js/Components/Frontend_ui/FrontendFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed bottom-0 z-40 left-0 bg-gray-900">
         <h1 class="dark:text-gray-100 text-white">
-            &copy; 2023 ThomasBockhorn.dev
+            &copy; 2023 ThomasBockhorn.com
         </h1>
     </div>
 </template>
